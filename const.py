6b53f267e3414bcd161d37657459ef4369c52902
antoninlefevre <antoninlefevre45@icloud.com>
2023-02-07 17:08:40 +0100

    init

---
+++ const.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Decoder(nn.Module):
+    """
+    Décode le vecteur latent et renvoie les données reconstruites
+    """
+
+    def __init__(self):
+        super(Decoder, self).__init__()
+        # Couches convolutives
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2)  # (1, 16, 1, 25)
+        self.convbatchNorm1 = nn.BatchNorm2d(16)
+        self.upsampling1 = nn.Upsample(size=(1, 250))  # (1, 16, 1, 250)
+        self.ConvUp1 = nn.ConvTranspose2d(16, 8, kernel_size=1, stride=1)  # (1, 8, 1, 250)
+        self.ConvUp2 = nn.ConvTranspose2d(8, 1, kernel_size=1, stride=1)  # (1, 1, 1, 250)
+        # Couches denses
+        self.dense1 = nn.Linear(25, 25)
+        self.dense2 = nn.Linear(25, 125)
+        self.dense3 = nn.Linear(125, 250)
+        # concaténation
+        self.concatDense1 = nn.Linear(500, 250)
+
+    def forward(self, z):
+        # Couches convolutives
+        z_conv = z.reshape((1, 25)).unsqueeze(0).unsqueeze(0)
+        z_conv = F.relu(self.convbatchNorm1(self.conv1(z_conv)))
+        z_conv = self.upsampling1(z_conv)
+        z_conv = self.ConvUp1(z_conv)
+        z_conv = self.ConvUp2(z_conv)
+        z_conv = z_conv.flatten()
+        # Couches denses
+        z_dense = self.dense1(z)
+        z_dense = F.relu(self.dense2(z_dense))
+        z_dense = F.relu(self.dense3(z_dense))
+        z_dense = z_dense.flatten()
+        # Concaténation
+        z_dense = torch.cat((z_conv, z_dense), 0)
+        # Dernières couches denses
+        z_final = self.concatDense1(z_dense)
+        z_final = z_final.reshape(1, 250)
+        return z_final
+
+
+diseases = ['normal sinus rhythm (NSR)', 'Atrial premature beat (APB)', 'Atrial flutter (AFL)',
+            'Atrial fibrillation (AFIB)', "Supraventricular tachyarrhythmia (SVTA)",
+            "Wolff-Parkinson-White syndrome (WPW)", "Premature ventricular contraction (PVC)",
+            "Ventricular bigeminy (Bigeminy)", "Ventricular trigeminy (Trigeminy)", "Ventricular tachycardia (VT)",
+            "Idioventricular rhythm (IVR)", "Ventricular flutter (VFL)",
+            "Fusion of ventricular and normal beat (Fusion)", "Left bundle branch block beat (LBBBB)",
+            "Right bundle branch block beat (RBBBB)", "Second-degree heart block (SDHB)", "Pacemaker rhythm (PR)"]
---
+++ main.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import torch
+import plotly.graph_objects as go
+from const import Decoder
+import pandas as pd
+
+st.set_page_config(layout="wide",
+                   page_title="ECG generator",
+                   menu_items={'About': "ECG generator - made by Antonin"},
+                   )
+# create css class to center the title "ECG generator"
+st.markdown(
+    """
+    <style>
+    .title {
+        font-size:75px !important;
+        font-weight: bold;
+        box-sizing: border-box;
+        text-align: left;
+        width: 100%;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+diseases = ['normal sinus rhythm (NSR)']
+
+st.markdown('<div class="title">ECG generator</div>', unsafe_allow_html=True)
+st.markdown('###')
+
+# Selector and slider
+col1, _, col2, _ = st.columns([1, 0.2, 1, 0.2])
+# create a selector to select a disease
+disease = col1.selectbox('Select a disease', diseases, index=0, help='Select a disease to generate ECGs')
+# create a slider to select the number of ECGs to generate
+number_of_ecgs = col2.slider('Number of ECGs to generate', min_value=1, max_value=500, value=10, step=1)
+st.markdown('###')
+
+# Button
+col1, _, col2, _ = st.columns([1, 0.2, 1, 0.2])
+# create a button to generate the ECGs, centered
+button = col1.button('Generate ECGs', key='button', help=f'Click to generate {number_of_ecgs} ECGs')
+st.markdown('###')
+
+# if the button is clicked
+if button:
+    # load the pytorch model at models/disease.pt
+    model = Decoder()
+    model.load_state_dict(torch.load(f'models/NSR_decoder_generation.pt'))
+    model.eval()
+    # generate the ECGs
+    generated_ECG = []
+    for i in range(number_of_ecgs):
+        with torch.no_grad():
+            latent_vector_shape = (1, 25)
+            # on échantillonne les distributions du vecteur latent pour en générer un nouveau
+            new_latent_ECG = torch.distributions.Normal(0, 1).sample(latent_vector_shape)
+            # on décode le vecteur latent pour obtenir un nouvel ECG
+            generated_ECG.append(model(new_latent_ECG).detach().numpy().flatten())
+    _, col1, _ = st.columns([0.1, 1, 0.1])
+    # plot with plotly all the generated ECGs on the same graph
+    fig = go.Figure()
+    for i in range(number_of_ecgs):
+        fig.add_trace(go.Scatter(y=generated_ECG[i]))
+    fig.update_layout(showlegend=False)
+    # transparent background
+    fig.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
+    col1.plotly_chart(fig)
+
+    # transform the generated ECGs into a pandas dataframe
+    generated_ECG = pd.DataFrame(generated_ECG).T
+    generated_ECG.columns = [f'ECG_{i}' for i in range(1, number_of_ecgs + 1)]
+    # download the generated ECGs as a csv file by transforming the dataframe into bytes
+    csv = generated_ECG.to_csv(index=False).encode()
+    # create a button to download the generated ECGs
+    col1.markdown('###')
+    col1.download_button(label='Download ECGs', data=csv, file_name='ECGs.csv', mime='text/csv')
